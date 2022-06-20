7849778ac82f0741212373292810a2f2ea49d22a
vishnuacappella <acappella.studiov@gmail.com>
2022-06-20 11:20:00 +0530

    const and final test upfated in bin folder

--- bin/const_vs_final.dart
+++ bin/const_vs_final.dart
@@ -1,5 +1,19 @@
-import 'package:const_vs_final/const_vs_final.dart' as const_vs_final;
-
 void main(List<String> arguments) {
-  print('Hello world: ${const_vs_final.calculate()}!');
+  final String employeeName;
+  employeeName = 'Vishnu M';
+  // employeeName = 'John';
+  //! final VARIABLE CAN ONLY SET ONCE
+
+  final List<int> employeeList;
+  employeeList = List.empty();
+  employeeList.add(1);
+  employeeList.add(2);
+  // employeeList = [1, 2, 3];
+  //! final LIST CAN ONLY SET ONCE BUT LIST VALUES CAN BE ADD OR REMOVE
+  final List<int> employeeList2 = [1, 2, 3];
+
+  const String studentName = 'Vishnu M';
+  // ! Adding an initialization to the declaration of a constant .
+  // const String name;
+  // name = 'Vishnu M';
 }
